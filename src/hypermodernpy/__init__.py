eba19f7740ac35047a2ee8017a37e8befb3fb868
Mohamed Abdellah <mohamed.ia@gmail.com>
2020-07-19 19:17:09 -0500

    Initial commit

---
+++ src/hypermodernpy/__init__.py
@@ -0,0 +1,2 @@
+# src/hypermodernpy/__init__.py
+__version__ = "0.1.0"
---
+++ src/hypermodernpy/console.py
@@ -0,0 +1,12 @@
+import click
+
+from . import __version__
+
+@click.command()
+
+@click.version_option(version=__version__)
+
+
+def main():
+	"""The hypermodenn Python project"""
+	click.echo("Hello, world!")
